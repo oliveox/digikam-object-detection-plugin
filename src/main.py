da2d8cb14dcb37aefb6adce971ebbd48d4e9a937
homosapiens <oliveox@protonmail.com>
2021-10-20 11:27:08 +0300

    Initialise plugin internal tables

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from services.utils import Utils
+from adapters.db import InternalDB
 import traceback
+
 
 if __name__ == "__main__":
     try:
+        #initialise environment
+        InternalDB.initialise_internal_db()
+
         # start analysis
         Utils.analyze_entities()
     except Exception as e:
